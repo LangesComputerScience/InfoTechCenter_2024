3b314b724c509bcf1146af5c79855070c5a8d162
LangesComputerScience <96296368+LangesComputerScience@users.noreply.github.com>
2024-02-21 14:08:48 -0500

    Updated the rest of the ELIF's and ELSE statements  - GitHub

--- GrimaceShake.py
+++ GrimaceShake.py
@@ -31,6 +31,16 @@ def gasLevelAlert():
         print("Your gas tank is extremely low, checking Google Maps for the closest gas station...")
         sleep(2.5)
         print("The closest gas station is",listOfGasStations(),"which is",milesToGasStationsLow,"miles away.")
+    elif gasLevelIndicator == "Quarter Tank":
+        print("Your gas tank is on a quarter tank, checking Google Maps for the closest gas station...")
+        sleep(2.5)
+        print("The closest gas station is",listOfGasStations(),"which is",milesToGasStationsQuarterTank,"miles away.")
+    elif gasLevelIndicator == "Half Tank":
+        print("Your gas tank is a half of a tank full which is plenty to get to your destination.")
+    elif gasLevelIndicator == "Three Quarter Tank":
+        print("Your gas tank is at three quarter tank.")
+    else:
+        print("Your gas tank is full - YEAH!!!! - Congratulations! - Vroom Vrooms!")
 
 
     
